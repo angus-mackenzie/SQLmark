5a82f2315738cc1967372134604a6d3f212aad38
Matthew Poulter <github@poulter.co.za>
2018-08-16 18:54:53 +0200

    Big update to models
    Added new WorkingData class to allow for static method calls to the database to get the working data for the assignment.

--- src/model/Database.java
+++ src/model/Database.java
@@ -1,14 +1,34 @@
 package model;
 
 import java.sql.ResultSet;
+import java.util.Map;
 
 public class Database {
     private String lastMessage;
     private CompileStatus lastStatus;
     private ResultSet lastResultSet;
+    private String currentSQL;
 
     public enum CompileStatus {
         SUCCESS, FAILURE
+    }
+
+    public Database() {
+        // TODO: Create new database connection with main database
+        throw new UnsupportedOperationException();
+    }
+
+    public Database(String databaseName) {
+        // TODO: Create new database connection with specified database
+        throw new UnsupportedOperationException();
+    }
+
+    public void prepareSelect(String table) {
+        prepareSelect(table, null);
+    }
+
+    public void prepareSelect(String table, Map<String, Object> where) {
+        prepareSelect(table, where, -1);
     }
 
     public void execute(String sql) {
@@ -31,8 +51,29 @@ public class Database {
         throw new UnsupportedOperationException();
     }
 
-    public void close() {
-        // TODO: Close resultset and DB connection
+    public void prepareSelect(String table, Map<String, Object> where, int limit) {
+        // TODO: Prepare SQL statement for select
         throw new UnsupportedOperationException();
     }
+
+    public void execute() {
+        execute(currentSQL);
+    }
+
+    public String exportToSQL() {
+        // TODO: Return the SQL command to recreate the table and contents of last query (null if no query run)
+        throw new UnsupportedOperationException();
+    }
+
+    public void close() {
+        // TODO: Close DB connection
+        throw new UnsupportedOperationException();
+    }
+
+    public void closeRS() {
+        // TODO: Close ResultSet
+        throw new UnsupportedOperationException();
+    }
+
+    // TODO: Create way to duplicate database and then delete it after query has been run
 }

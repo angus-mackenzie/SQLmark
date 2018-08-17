deab1cba377c3b777f50634307e77088ee7bcfa7
Matthew Poulter <github@poulter.co.za>
2018-08-17 13:09:17 +0200

    Fixes to BufferedReader in Student view

--- src/view/Student.java
+++ src/view/Student.java
@@ -8,39 +8,33 @@ import java.io.InputStreamReader;
 
 public class Student {
     private controller.Student student;
-    private BufferedReader br;
 
     public Student(controller.Student student) {
         this.student = student;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            this.br = br;
 
-            boolean running = true;
-            while (running) {
-                showMenu();
-                int menuItem = Integer.parseInt(getReturn());
-                switch (menuItem) {
-                    case 1:
-                        System.out.println(this.student.loadAssignment());
-                        break;
-                    case 2:
-                        System.out.println(this.student.getData());
-                        break;
-                    case 3:
-                        runAssignment();
-                        break;
-                    case 9:
-                        running = false;
-                        break;
-                }
+        boolean running = true;
+        while (running) {
+            showMenu();
+            switch (getReturn()) {
+                case "1":
+                    System.out.println(this.student.loadAssignment());
+                    break;
+                case "2":
+                    System.out.println(this.student.getData());
+                    break;
+                case "3":
+                    runAssignment();
+                    break;
+                case "9":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Please enter valid option!");
+                    break;
             }
-
-            System.out.println("Goodbye!");
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        System.out.println("Goodbye!");
     }
 
     private void showMenu() {
@@ -53,11 +47,16 @@ public class Student {
         System.out.print("Enter option: ");
     }
 
-    private String getReturn() throws IOException {
-        return br.readLine();
+    private String getReturn() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            return br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    private void runAssignment() throws IOException {
+    private void runAssignment() {
         student.createSubmission();
 
         String question;

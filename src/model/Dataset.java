a49148a1604039fea2902efd4d02ecaa172f2365
Matthew Poulter <github@poulter.co.za>
2018-09-06 23:52:06 +0200

    Compare datasets done (minus testing)

--- src/model/Dataset.java
+++ src/model/Dataset.java
@@ -9,41 +9,20 @@ import java.util.List;
 
 /**
  * A dataset that holds a result for a question/answer
+ *
  * @author Matthew Poulter
  * @version 15/09/2018
  */
 public class Dataset {
     private List<List<Object>> dataset;
+    private List<Dataset> datasets;
+    private Integer rowsUpdated;
     private String compileMessage;
     private Database.CompileStatus compileStatus;
 
     /**
-     * Compares two datasets to one another
-     * @param dataset to be compared to
-     * @return true or false
-     */
-    public boolean equals(Dataset dataset) {
-        return Arrays.deepEquals(dataset.convertList(), convertList());
-    }
-
-    /**
-     * Return the compile time message
-     * @return compile message
-     */
-    public String getCompileMessage() {
-        return compileMessage;
-    }
-
-    /**
-     * Returns the compile status
-     * @return compile status
-     */
-    public Database.CompileStatus getCompileStatus() {
-        return compileStatus;
-    }
-
-    /**
      * Creates a dataSet with the given SQL statement
+     *
      * @param sql to be executed
      * @throws Error if it cannot connect to DB
      */
@@ -62,7 +41,12 @@ public class Dataset {
                     this.dataset = convertResultSet(db.getResultSet());
                     db.closeRS();
                 } else {
-                    // TODO: INSERT, UPDATE, OR DELETE
+                    this.rowsUpdated = db.getUpdateCount();
+                    this.datasets = new ArrayList<>();
+
+                    for (String table : db.getAllTables()) {
+                        this.datasets.add(new Dataset("SELECT * FROM " + table + ";"));
+                    }
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -75,12 +59,54 @@ public class Dataset {
     }
 
     /**
+     * Compares two datasets to one another
+     *
+     * @param dataset to be compared to
+     * @return true or false
+     */
+    public boolean equals(Dataset dataset) {
+        if (rowsUpdated == null && dataset.rowsUpdated == null && Arrays.deepEquals(dataset.convertList(), convertList())) {
+            return true;
+        } else if (rowsUpdated != null && dataset.rowsUpdated != null && rowsUpdated.equals(dataset.rowsUpdated) && datasets.size() == dataset.datasets.size()) {
+            boolean check = true;
+
+            for (int i = 0; i < datasets.size(); i++) {
+                if (!datasets.get(i).equals(dataset.datasets.get(i))) {
+                    check = false;
+                    break;
+                }
+            }
+
+            return check;
+        }
+        return false;
+    }
+
+    /**
+     * Return the compile time message
+     *
+     * @return compile message
+     */
+    public String getCompileMessage() {
+        return compileMessage;
+    }
+
+    /**
+     * Returns the compile status
+     *
+     * @return compile status
+     */
+    public Database.CompileStatus getCompileStatus() {
+        return compileStatus;
+    }
+
+    /**
      * Creates a list of the dataset
      *
      * @return an Object[][] array, null if there are no values in the dataset
      */
     private Object[][] convertList() {
-        if(!(dataset==null)){
+        if (!(dataset == null)) {
             Object[][] array = new Object[dataset.size()][];
             for (int i = 0; i < dataset.size(); i++) {
                 List<Object> row = dataset.get(i);
@@ -93,6 +119,7 @@ public class Dataset {
 
     /**
      * Creates a string representation of the data
+     *
      * @return output
      */
     @Override
@@ -112,6 +139,7 @@ public class Dataset {
 
     /**
      * Converts a result set to a list
+     *
      * @param rs result set
      * @return a list of the result set
      * @throws SQLException
